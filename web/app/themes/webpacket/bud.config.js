d558a63fd664452d58dd6a206c40db3b7cf26745
pkosciak <pkosciak@webpacket.io>
2024-04-28 16:16:58 +0200

    Add sage theme

---
+++ web/app/themes/webpacket/bud.config.js
@@ -0,0 +1,87 @@
+/**
+ * Compiler configuration
+ *
+ * @see {@link https://roots.io/sage/docs sage documentation}
+ * @see {@link https://bud.js.org/learn/config bud.js configuration guide}
+ *
+ * @type {import('@roots/bud').Config}
+ */
+export default async (app) => {
+  /**
+   * Application assets & entrypoints
+   *
+   * @see {@link https://bud.js.org/reference/bud.entry}
+   * @see {@link https://bud.js.org/reference/bud.assets}
+   */
+  app
+    .entry('app', ['@scripts/app', '@styles/app'])
+    .entry('editor', ['@scripts/editor', '@styles/editor'])
+    .assets(['images']);
+
+  /**
+   * Set public path
+   *
+   * @see {@link https://bud.js.org/reference/bud.setPublicPath}
+   */
+  app.setPublicPath('/app/themes/webpacket/public/');
+
+  /**
+   * Development server settings
+   *
+   * @see {@link https://bud.js.org/reference/bud.setUrl}
+   * @see {@link https://bud.js.org/reference/bud.setProxyUrl}
+   * @see {@link https://bud.js.org/reference/bud.watch}
+   */
+  app
+    .setUrl('http://localhost:3000')
+    .setProxyUrl('https://bedrock.test')
+    .watch(['resources/views', 'app']);
+
+  app.serve({
+    ssl: true,
+    host: 'bedrock.test',
+    cert: app.path('local-cert.pem'),
+    key: app.path('local-key.pem'),
+  });
+
+  /**
+   * Generate WordPress `theme.json`
+   *
+   * @note This overwrites `theme.json` on every build.
+   *
+   * @see {@link https://bud.js.org/extensions/sage/theme.json}
+   * @see {@link https://developer.wordpress.org/block-editor/how-to-guides/themes/theme-json}
+   */
+  app.wpjson
+    .setSettings({
+      background: {
+        backgroundImage: true,
+      },
+      color: {
+        custom: false,
+        customDuotone: false,
+        customGradient: false,
+        defaultDuotone: false,
+        defaultGradients: false,
+        defaultPalette: false,
+        duotone: [],
+      },
+      custom: {
+        spacing: {},
+        typography: {
+          'font-size': {},
+          'line-height': {},
+        },
+      },
+      spacing: {
+        padding: true,
+        units: ['px', '%', 'em', 'rem', 'vw', 'vh'],
+      },
+      typography: {
+        customFontSize: false,
+      },
+    })
+    .useTailwindColors()
+    .useTailwindFontFamily()
+    .useTailwindFontSize();
+};
